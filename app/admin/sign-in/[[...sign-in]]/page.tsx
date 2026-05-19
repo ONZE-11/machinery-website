4bd20f968b1d30204f27193abd3c2a882f30822e
ONZE-11 <mahjoubia509@gmail.com>
2026-05-18 23:03:20 -0400

    fix clerk sign in page

--- app/admin/sign-in/[[...sign-in]]/page.tsx
+++ app/admin/sign-in/[[...sign-in]]/page.tsx
@@ -7,7 +7,6 @@ export default function Page() {
         routing="path"
         path="/admin/sign-in"
         forceRedirectUrl="/admin"
-        fallbackRedirectUrl="/admin"
       />
     </div>
   )

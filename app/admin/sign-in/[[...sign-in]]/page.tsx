44b710877df44f15c8546fa7730b21086708d57b
ONZE-11 <mahjoubia509@gmail.com>
2026-05-18 23:28:10 -0400

    wrap admin sign in with clerk provider

--- app/admin/sign-in/[[...sign-in]]/page.tsx
+++ app/admin/sign-in/[[...sign-in]]/page.tsx
@@ -1,13 +1,16 @@
-import { SignIn } from "@clerk/nextjs"
+import { ClerkProvider, SignIn } from "@clerk/nextjs"
 
 export default function Page() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <SignIn
-        routing="path"
-        path="/admin/sign-in"
-        forceRedirectUrl="/admin"
-      />
-    </div>
+    <ClerkProvider>
+      <div className="flex min-h-screen items-center justify-center">
+        <SignIn
+          routing="path"
+          path="/admin/sign-in"
+          forceRedirectUrl="/admin"
+          fallbackRedirectUrl="/admin"
+        />
+      </div>
+    </ClerkProvider>
   )
 }
